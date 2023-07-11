309ed0d68221ffaef646d43e83b87f1cfa4aa619
DOS REIS Vincent <vincent.dosreis@orange.com>
2023-07-11 15:45:14 +0200

    .

--- les_pages/dataviz.py
+++ les_pages/dataviz.py
@@ -7,11 +7,13 @@ import seaborn as sns
 
 
 url_incident="https://data.london.gov.uk/download/london-fire-brigade-incident-records/73728cf4-b70e-48e2-9b97-4e4341a2110d/LFB%20Incident%20data%20-%20Datastore%20-%20with%20notional%20cost%20and%20UPRN%20from%20January%202009.zip"
+url_incident="incident.csv"
 @st.cache_data
 def charge_data_incident():
     return pd.read_csv(url_incident, compression='zip', low_memory=False)
 
 url_mobilisation="https://data.london.gov.uk/download/london-fire-brigade-mobilisation-records/fcbd2e97-b5bf-4117-a50f-d596181bc8d3/LFB%20Mobilisation%20data%20from%20January%202009.zip"
+url_mobilisation="mobilisation.csv"
 @st.cache_data
 def charge_data_mobilisation():
     return pd.read_csv(url_mobilisation, compression='zip', low_memory=False)
